46a2eae4f170c93aa032a224a1b44d5c71592625
niloyahmedrasel <niloyahmedrasel24@gmail.com>
2024-11-10 22:22:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import Post from './component/post';
 
 function App() {
+  const [data,setData] = useState([])
+ 
+ useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/photos?_limit=10')
+  .then(res=>res.json())
+  .then(data=>setData(data))
+ },[])
+ 
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className='text-green-500 text-4xl'>This is my first API call to create cards</h1>
+        <div className='grid lg:grid-cols-3 gap-4 mt-5'>
+        {
+          data.map((element,index)=><Post key={index} element={element}></Post>)
+        }
+        </div>
+        
+      
     </div>
   );
 }
 
 export default App;
+
+
+
